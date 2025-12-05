b1a5e909c41a2b8db2311cd50b31afbee50e2f3e
Keith C. Canty <keith.curtis.canty@gmail.com>
2025-12-04 23:20:22 -0700

    Update main.py
    if / elif statements added for logical conditions

--- beginners/calculator/main.py
+++ beginners/calculator/main.py
@@ -3,9 +3,18 @@
 # User will select operator (+ - * /) and input two values when prompted.
 # Value of two values will be calculated based on operand selected.
 
-operand = input("Select operator: + - * /")
-num1 = input("Choose value 1: ")
-num2 = input("Choose value 2: ")
+operand = input("Select operator: (+ - * /"))
+num1 = float(input("Enter value 1: ")) # typecast as float for more complex calculations
+num2 = float(input("Enter value 2: "))
 
-print(num1 {operand) num2)
+if operand == "+":
+  print(num1 + num2)
+elif operand == "-":
+  print(num1 - num2)
+elif operand == "*":
+  print(num1 * num2)
+elif operand == "/":
+  print(num1 / num2)
+
+
 
