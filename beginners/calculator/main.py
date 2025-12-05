a5ce2ac82adbac974bbed7faeff2891b78accf13
Keith C. Canty <keith.curtis.canty@gmail.com>
2025-12-04 21:00:08 -0700

    Update main.py
    Project and outcome defined.
    
    User input prompts added.

--- beginners/calculator/main.py
+++ beginners/calculator/main.py
@@ -1,1 +1,11 @@
+# Mathematical Operators
+
+# User will select operator (+ - * /) and input two values when prompted.
+# Value of two values will be calculated based on operand selected.
+
+operand = input("Select operator: + - * /")
+num1 = input("Choose value 1: ")
+num2 = input("Choose value 2: ")
+
+print(num1 {operand) num2)
 
