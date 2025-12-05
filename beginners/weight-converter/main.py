e4facee1c19ea34ef753f8e9cccccdef14f1f45d
Keith C. Canty <keith.curtis.canty@gmail.com>
2025-12-05 01:03:52 -0700

    Update main.py
    Output statement moved within if / elif statements so if invalid entry is made by user (anything other than the requests 'kg' or 'lb' only the else output is generated.

--- beginners/weight-converter/main.py
+++ beginners/weight-converter/main.py
@@ -5,9 +5,11 @@ unit = input("Is weight measured in kilograms or pounds? (Enter kg or lb): ")
 
 if unit == "kg":
   weight = weight * 2.205
+  print(f:"Your weight is: {round{weight, 2} {unit}") # round function added, unit will be round to 2 decimal places
 elif unit == "lb":
   weight = weight / 2.205
+  print(f:"Your weight is: {round{weight, 2} {unit}") # round function added, unit will be round to 2 decimal places
 else:
   print(f"{unit} isn't valid")
 
-print(f:"Your weight is: {round{weight, 2} {unit}") # round function added, unit will be round to 2 decimal places
+
